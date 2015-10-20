78d6bd4a7fb214f24343d186c4fab40ff4ca27ab
Radu Vadim <raduvadim@gmail.com>
2015-10-20 12:25:15 +0300

    first software upload
    restructured directory tree, commit barebone max7219 example

---
+++ software/numi_clock/numi_clock/numi_clock.ino
@@ -0,0 +1,74 @@
+
+#include "LedControl.h"
+#define DataIn 11
+#define Clk 13
+#define Load 15
+/*
+ Now we need a LedControl to work with.
+ pin 11 is connected to the DataIn (pin PB3)
+ pin 13 is connected to the CLK (pin PB5)
+ pin 15 is connected to LOAD (pin PC1) 
+ We have only a single MAX72XX.
+ */
+LedControl lc=LedControl(DataIn,Clk,Load,1);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=250;
+
+void setup() {
+  /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+}
+
+
+/*
+ This method will display the characters for the
+ word "Arduino" one after the other on digit 0. 
+ */
+void writeArduinoOn7Segment() {
+  lc.setChar(0,0,'a',false);
+  delay(delaytime);
+  lc.setRow(0,0,0x05);
+  delay(delaytime);
+  lc.setChar(0,0,'d',false);
+  delay(delaytime);
+  lc.setRow(0,0,0x1c);
+  delay(delaytime);
+  lc.setRow(0,0,B00010000);
+  delay(delaytime);
+  lc.setRow(0,0,0x15);
+  delay(delaytime);
+  lc.setRow(0,0,0x1D);
+  delay(delaytime);
+  lc.clearDisplay(0);
+  delay(delaytime);
+} 
+
+/*
+  This method will scroll all the hexa-decimal
+ numbers and letters on the display. You will need at least
+ four 7-Segment digits. otherwise it won't really look that good.
+ */
+void scrollDigits() {
+  for(int i=0;i<13;i++) {
+    lc.setDigit(0,3,i,false);
+    lc.setDigit(0,2,i+1,false);
+    lc.setDigit(0,1,i+2,false);
+    lc.setDigit(0,0,i+3,false);
+    delay(delaytime);
+  }
+  lc.clearDisplay(0);
+  delay(delaytime);
+}
+
+void loop() { 
+  writeArduinoOn7Segment();
+  scrollDigits();
+}
